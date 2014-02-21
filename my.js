6e37353d88dbceb9124d24cdedb692b1535dad14
tmt <tomita.otwo@gmail.com>
2014-02-21 17:08:50 +0900

    idol数を200に変更

--- my.js
+++ my.js
@@ -206,7 +206,7 @@ $(function() {
             CTaskManager.getInstance().start();
             new CBackgroundImageTask();
 
-            for ( var i=0; i<150; i++ )
+            for ( var i=0; i<200; i++ )
             {
                 new CIdolTask();
             }
