77f98581e12212a184521c6783fa42a215c71481
tmt <tomita.otwo@gmail.com>
2014-02-22 23:32:44 +0900

    リダイレクト用コードを修正

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ header("Content-Type: text/html; charset=文字コード");
 header("Expires: Thu, 01 Dec 1994 16:00:00 GMT");
 header("Last-Modified: ". gmdate("D, d M Y H:i:s"). " GMT");
 header("Cache-Control: no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
+// header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
 header('Content-Type: application/javascript');
