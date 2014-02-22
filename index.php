91c0e89788dd596803d43c76d167a670532ce7dd
tmt <tomita.otwo@gmail.com>
2014-02-22 23:02:55 +0900

    リダイレクト用コードを追加

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo 'Hello World!';
 
+
+$jsurl = $_GET['jsurl'];
+
+
+if ( !preg_match('/^[a-z0-9\-]+.js$/', $jsurl) ){
+	echo 'Hello World!';
+	exit;
+
+}
+
+$jspath = './' . $jsurl;
+if ( !file_exists($jspath)){
+	echo 'Hello World!!';
+	exit;
+}
+
+header('Location:http://idl-prt-js.herokuapp.com/' . $jsurl . '?' . time() );
+
+exit;
