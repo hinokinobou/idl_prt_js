8041fb6f79622e98f858f9dc1999f872d53135f1
tmt <tomita.otwo@gmail.com>
2014-02-24 13:34:59 +0900

    hideStatusBarをiOSのみに適用させる

--- my.js
+++ my.js
@@ -277,5 +277,7 @@ $(function() {
 }); // on DOMContentLoaded
 
 
-
-window.StatusBar.hideStatusBar();
+//iOSはhideStatusBarを呼ぶ
+if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0){
+    window.StatusBar.hideStatusBar();
+}
